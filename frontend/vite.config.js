d538bad9b0c349d332302f7d19725d375717c33f
Afifa Fatima <afifarizvi7@gmail.com>
2025-10-10 23:41:21 +0530

    cors update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    headers: {
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
-    }
-  },
   optimizeDeps: {
   include: ["react-toastify"]
 }
