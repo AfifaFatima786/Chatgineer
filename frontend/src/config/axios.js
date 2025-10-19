1ff3e6766a71f3ff435ab14bc9a79714d8cb7ef8
Afifa Fatima <143158910+AfifaFatima786@users.noreply.github.com>
2025-10-20 00:24:13 +0530

    Log API Base URL in axios configuration
    Added console log for API base URL in axios configuration.

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+console.log('API Base URL:', import.meta.env.VITE_API_PATH) 
 const axiosInstace=axios.create({
     baseURL:import.meta.env.VITE_API_PATH,
 })
+
 
 export default axiosInstace
