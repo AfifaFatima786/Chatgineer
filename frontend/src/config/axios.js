8d0352775f4f66f56b26be00809e366e5c986a7d
Afifa Fatima <143158910+AfifaFatima786@users.noreply.github.com>
2025-10-20 15:11:18 +0530

    Add withCredentials option to axios instance

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 console.log('API Base URL:', import.meta.env.VITE_API_PATH) 
 const axiosInstace=axios.create({
     baseURL:import.meta.env.VITE_API_PATH,
+    withCredentials:true
 })
 
 
 export default axiosInstace
+
